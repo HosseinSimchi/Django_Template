6e2fe80753aead2e5fde47ea9fc96df81117d1e8
Hossein Simchi <95399810+HosseinSimchi@users.noreply.github.com>
2022-05-20 14:49:05 +0430

    We completed Templates in Django

--- MyApp/urls.py
+++ MyApp/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
-from MyApp.views import index_view
+from MyApp.views import index_view,innerpage_view,PortFolio_view
 
 urlpatterns = [
-    path('Home', index_view),
+    path('', index_view, name = "index"),
+    path('Page1', innerpage_view, name = "InnerPage"),
+    path('Page2', PortFolio_view, name = "PortfolioDetails")
 ]
--- MyApp/views.py
+++ MyApp/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def index_view(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
+def innerpage_view(request):
+    return render(request, 'inner-page.html')
+
+def PortFolio_view(request):
+    return render(request, 'portfolio-details.html')
